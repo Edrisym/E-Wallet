1a3efe789859dff870efd7ff2ae531b6caaebb05
Eddie <edrismoavenghafouri@gmail.com>
2024-11-12 20:39:21 +0330

    Creating test for domain and adding If_Balance_Is_Negative test

--- Wallet_Tests/Wallet_Domain_Tests.cs
+++ Wallet_Tests/Wallet_Domain_Tests.cs
@@ -1,9 +1,42 @@
+using FluentAssertions;
+
 namespace Wallet_Tests;
 
 public class Wallet_Domain_Tests
 {
+    // TODO -- testing steps
+    //Arrange & Act 
+    //assert
+
     [Fact]
-    public void Test1()
+    public void Should_Throw_Exception_If_Balance_Is_Negative()
     {
+        var balance = -0.75m;
+        var guidId = Guid.NewGuid();
+        
+        var walletCreation = () => new Wallet(guidId, balance);
+        
+        walletCreation.Should().ThrowExactly<NegativeBalanceException>();
+    }
+}
+
+public class Wallet
+{
+    public Wallet()
+    {
+    }
+
+    public Guid Id { get; private set; }
+    public decimal Balance { get; private set; }
+
+    public Wallet(Guid id, decimal balance)
+    {
+        if (decimal.IsNegative(balance))
+        {
+            NegativeBalanceException.Throw(balance);
+        }
+
+        Id = id;
+        Balance = balance;
     }
 }
