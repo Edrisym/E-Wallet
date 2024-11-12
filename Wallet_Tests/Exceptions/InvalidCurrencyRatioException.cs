38582970f1a06cfc95dffe23d0064ba7d64bef1d
Eddie <edrismoavenghafouri@gmail.com>
2024-11-12 22:41:42 +0330

    creating InvalidCurrencyRatioException

---
+++ Wallet_Tests/Exceptions/InvalidCurrencyRatioException.cs
@@ -0,0 +1,12 @@
+namespace Wallet_Tests.Exceptions;
+
+public class InvalidCurrencyRatioException(decimal ratio) : Exception(string.Format(Message, ratio))
+{
+    private new const string Message = "The currency ratio {0} is invalid. It must be positive and equal to 1.0.";
+
+    [DoesNotReturn]
+    public static void Throw(decimal balance)
+    {
+        throw new InvalidCurrencyRatioException(balance);
+    }
+}
