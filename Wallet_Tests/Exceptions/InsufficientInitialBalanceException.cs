d43b842484c8f9f8eb2de6c3d9c299b5373a85f4
Eddie <edrismoavenghafouri@gmail.com>
2024-11-13 18:50:54 +0330

    Creating InsufficientInitialBalanceException

---
+++ Wallet_Tests/Exceptions/InsufficientInitialBalanceException.cs
@@ -0,0 +1,14 @@
+namespace Wallet_Tests;
+
+public class InsufficientInitialBalanceException(decimal balance) :
+    Exception(string.Format(Message, balance))
+{
+    private new const string Message =
+        "Insufficient balance {0}. Initial balance is insufficient. Please meet the required minimum.";
+
+    [DoesNotReturn]
+    public static void Throw(decimal balance)
+    {
+        throw new InsufficientInitialBalanceException(balance);
+    }
+}
