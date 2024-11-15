fb581b5b3d3177203e6c9db322dfb680bd93921c
Eddie <edrismoavenghafouri@gmail.com>
2024-11-15 19:49:09 +0330

    configuring globalusings

--- EWallet.Api/UsingGlobals.cs
+++ EWallet.Api/UsingGlobals.cs
@@ -1,2 +1,8 @@
 global using System.Diagnostics.CodeAnalysis;
 global using EWallet.Api.Exceptions;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.Filters;
+global using System.Text;
+global using EWallet.Api;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.IdentityModel.Tokens;
