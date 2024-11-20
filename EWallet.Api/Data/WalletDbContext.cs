d635c2ac6963aa6cd6d78c6d1efd988a5d98be91
Eddie <edrismoavenghafouri@gmail.com>
2024-11-20 12:18:54 +0330

    Creating Wallet DbContext

--- EWallet.Api/Data/WalletDbContext.cs
+++ EWallet.Api/Data/WalletDbContext.cs
@@ -1,6 +1,61 @@
-namespace EWallet.Api.Data;
 
-public class WalletDbContext : dbcontext
+public class WalletDbContext(DbContextOptions<WalletDbContext> options) : DbContext(options)
 {
-    // TODO ** Create ** 
+    public DbSet<Wallet> Wallets { get; set; }
+    public DbSet<Currency> Currencies { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Currency>(builder =>
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.CreatedOnUtc)
+                .IsRequired();
+
+            builder.Property(x => x.ModifiedOnUtc)
+                .IsRequired(false);
+
+            builder.Property(x => x.Code)
+                .HasMaxLength(10) //BASEDGODS,HOTDOGE
+                .IsUnicode(false);
+
+            builder.Property(x => x.Name) // BTC,USD,EUR
+                .HasMaxLength(100)
+                .IsUnicode(false);
+
+            builder.HasIndex(c => c.Code)
+                .HasDatabaseName("IX_Currencies_Code")
+                .IsClustered(false);
+        });
+
+        modelBuilder.Entity<Wallet>(entity =>
+        {
+            entity.ToTable("Wallets");
+
+            entity.HasKey(w => w.Id);
+
+            entity.Property(w => w.Balance)
+                .IsRequired()
+                .HasColumnType("decimal(18, 2)");
+
+            entity.Property(w => w.StatusId)
+                .IsRequired();
+
+            entity.Property(w => w.CreatedOnUtc)
+                .IsRequired();
+
+            entity.Property(w => w.ModifiedOnUtc)
+                .IsRequired(false);
+
+            entity.HasOne(w => w.Currency)
+                .WithMany()
+                .HasForeignKey(w => w.CurrencyId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            entity.HasIndex(w => w.StatusId)
+                .HasDatabaseName("IX_Wallets_StatusId")
+                .IsClustered(false);
+        });
+    }
 }
