d62ac85439245ee18a860a3173ea9ab8cff268f1
Eddie <edrismoavenghafouri@gmail.com>
2024-11-20 13:49:36 +0330

    Adding static data for table schema name and default db value

--- EWallet.Api/Common/StaticData.cs
+++ EWallet.Api/Common/StaticData.cs
@@ -1,9 +1,22 @@
 namespace EWallet.Api.Common;
 
-public class StaticData
-{   
+public abstract class StaticData
+{
     public static class ConnectionStrings
     {
         public const string Default = "Default";
+    }
+    
+    public static class TableName
+    {
+        public const string Wallets = "Wallets";
+        public const string Currencies = "Currencies";
+    }
+
+    public static class WalletDbContextDefaultColumnType
+    {
+        public const string Decimal = "DECIMAL(18, 2)";
+        public const string Nvarchar36 = "NVARCHAR(36)";
+
     }
 }
