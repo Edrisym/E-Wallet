7da0f69e5d953388595ffd78f6686ce7fb2d0967
Eddie <edrismoavenghafouri@gmail.com>
2024-11-20 18:17:11 +0330

    Updating sql Sql-Script

--- EWallet.Api/Data/Sql-Script.sql
+++ EWallet.Api/Data/Sql-Script.sql
@@ -4,18 +4,18 @@ CREATE TABLE [Currencies] (
     [Id] NVARCHAR(36) NOT NULL,
     [Name] varchar(100) NOT NULL,
     [Code] varchar(10) NOT NULL,
-    [Ratio] decimal(18,2) NOT NULL,
-    [ModifiedOnUtc] datetime2 NOT NULL,
+    [Ratio] DECIMAL(18,2) NOT NULL,
+    [ModifiedOnUtc] datetime2 NULL,
     [CreatedOnUtc] datetime2 NOT NULL,
     CONSTRAINT [PK_Currencies] PRIMARY KEY ([Id])
     );
 
 CREATE TABLE [Wallets] (
     [Id] uniqueidentifier NOT NULL,
-    [Balance] decimal(18,2) NOT NULL,
+    [Balance] DECIMAL(18,2) NOT NULL,
     [StatusId] int NOT NULL,
     [CurrencyId] NVARCHAR(36) NOT NULL,
-    [ModifiedOnUtc] datetime2 NOT NULL,
+    [ModifiedOnUtc] datetime2 NULL,
     [CreatedOnUtc] datetime2 NOT NULL,
     [Status] int NOT NULL,
     CONSTRAINT [PK_Wallets] PRIMARY KEY ([Id]),
@@ -27,8 +27,5 @@ CREATE NONCLUSTERED INDEX [IX_Currencies_Code] ON [Currencies] ([Code]);
 CREATE INDEX [IX_Wallets_CurrencyId] ON [Wallets] ([CurrencyId]);
 
 CREATE NONCLUSTERED INDEX [IX_Wallets_StatusId] ON [Wallets] ([StatusId]);
-
 COMMIT;
 GO
-
-
