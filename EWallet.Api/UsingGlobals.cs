1fc479a7229cbac71352612cba9cec3eee26d6de
Eddie <edrismoavenghafouri@gmail.com>
2024-11-20 12:19:00 +0330

    Usings

--- EWallet.Api/UsingGlobals.cs
+++ EWallet.Api/UsingGlobals.cs
@@ -1,8 +1,25 @@
 global using System.Diagnostics.CodeAnalysis;
+global using System.Text;
+
+#region Microsoft
+
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Mvc.Filters;
-global using System.Text;
-global using EWallet.Api.Common;
-global using Microsoft.AspNetCore.Authentication.JwtBearer;
 global using Microsoft.IdentityModel.Tokens;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+#endregion
+
+#region EWallet
+
+global using EWallet.Api.Common;
 global using EWallet.Api.Common.Exceptions;
+global using EWallet.Api.Common.Models;
+
+#endregion
+
+#region DB
+
+global using Microsoft.EntityFrameworkCore;
+
+#endregion
