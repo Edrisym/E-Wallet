4ed792cb70081329909c6d71fa1778eb271383de
Eddie <edrismoavenghafouri@gmail.com>
2024-11-20 13:27:48 +0330

    Made some changes to make it more readable

--- EWallet.Api/Common/Models/CurrencyId.cs
+++ EWallet.Api/Common/Models/CurrencyId.cs
@@ -8,29 +8,17 @@ public class CurrencyId(Guid value)
     /// Generates a new unique CurrencyId.
     /// </summary>
     /// <returns>A new CurrencyId instance with a unique GUID.</returns>
-    public static CurrencyId NewId()
-    {
-        return new CurrencyId(Guid.NewGuid());
-    }
+    public static CurrencyId NewId() => new CurrencyId(Guid.NewGuid());
 
     /// <summary>
     /// Creates a CurrencyId from an existing GUID.
     /// </summary>
     /// <param name="value">The GUID value.</param>
     /// <returns>A CurrencyId instance.</returns>
-    public static CurrencyId From(Guid value)
-    {
-        if (value == Guid.Empty)
-            throw new ArgumentException("CurrencyId cannot be an empty GUID.", nameof(value));
-
-        return new CurrencyId(value);
-    }
+    public static CurrencyId From(Guid value) => new CurrencyId(value);
 
     /// <summary>
     /// Overrides ToString to return the GUID as a string.
     /// </summary>
-    public override string ToString()
-    {
-        return Value.ToString();
-    }
+    public override string ToString() => Value.ToString();
 }
