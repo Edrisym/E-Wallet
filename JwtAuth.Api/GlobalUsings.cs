4024a2d0d665fa0094b1776969c0511c5b6830e8
Eddie <edrismoavenghafouri@gmail.com>
2024-11-24 13:56:04 +0330

    usings

---
+++ JwtAuth.Api/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using System.IdentityModel.Tokens.Jwt;
+global using System.Security.Claims;
+global using System.Text;
+global using Microsoft.IdentityModel.Tokens;
+global using Microsoft.AspNetCore.Mvc;
