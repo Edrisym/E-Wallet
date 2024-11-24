9c7188c852394196539a11c6a3c174fd9ebd9a7d
Eddie <edrismoavenghafouri@gmail.com>
2024-11-24 13:55:26 +0330

    Usings

--- EWallet.Api/UsingGlobals.cs
+++ EWallet.Api/UsingGlobals.cs
@@ -19,8 +19,8 @@ global using EWallet.Api.Common.Models;
 global using DefaultColumnType = EWallet.Api.Common.StaticData.WalletDbContextDefaultColumnType;
 global using SchemaTable = EWallet.Api.Common.StaticData.TableName;
 global using ErrorMessages = EWallet.Api.Common.StaticData.ErrorMessages;
-global using EWallet.Api.Common.Currencies.EndPoints;
-global using EWallet.Api.Common.Wallets.EndPoints;
+global using EWallet.Api.Currencies.EndPoints;
+global using EWallet.Api.Wallets.EndPoints;
 
 #endregion
 
