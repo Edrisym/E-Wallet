2f25b76e751212dc7f36de7beeb9c339cdcac7cf
Eddie <edrismoavenghafouri@gmail.com>
2024-12-07 20:46:40 +0330

    [added] Creating transaction dbset

--- EWallet.Api/Data/WalletDbContext.cs
+++ EWallet.Api/Data/WalletDbContext.cs
@@ -4,6 +4,7 @@ public class WalletDbContext(DbContextOptions<WalletDbContext> options) : DbCont
 {
     public DbSet<Wallet> Wallets { get; set; }
     public DbSet<Currency> Currencies { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
